c278cc87a12e6a68eca7feeaeedfd2cd69a0a5b8
cloudjerry <34893085+cloudjerry@users.noreply.github.com>
2023-11-26 00:32:51 -0600

    Update bedrock.py

--- bedrock.py
+++ bedrock.py
@@ -1,4 +1,33 @@
 # RAG with bedrock AWS
 
+A simple RAG LangChain Implementation
 
+
+# Import library/package
+From 
+
+
+
+# Load document
+
+
+
+# Spilt doc into smaller chunk before embedding
+
+
+
+
+# Create Embedding client using bedrock Titan Embedding Model
+
+
+
+# Create vector store, embeddings and derive the an index for the document
+
+
+
+# Create the main text LLM for reasoning summarization QA
+
+
+
+# Invoke LLM -Search Query against Index, provide that as context to LLM
 
